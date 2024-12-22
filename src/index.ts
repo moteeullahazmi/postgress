7ec94460eaad7e09b69fd48017d9b599f2bebcdc
moteeullahazmi <moteeullahazmi@gmail.com>
2024-12-22 21:03:50 +0530

    127

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Moteeullah Azmi")
+import {Client} from "pg";
+const pgClient = new Client("postgresql://neondb_owner:lDE72uVYQNBq@ep-empty-glade-a5fof37v.us-east-2.aws.neon.tech/neondb?sslmode=require");
+
+
+async function pgClientConnect(){
+    await pgClient.connect();
+    const response = await pgClient.query("UPDATE users SET username= 'moteeullah.azmi' WHERE id='2' ");
+    console.log(response.rows) // response.rows
+}
+
+pgClientConnect();
